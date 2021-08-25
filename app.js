b0a656b2d8f690618049a24e17baad7a4721e52f
Nia Murrell (NBCUniversal) <Nia.Murrell@nbcuni.com>
2021-08-25 15:51:14 +0100

    Set up 'routes' directory

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express();
 const nodeEnv = process.env.NODE_ENV || 'development';
 if (nodeEnv === 'development') require('dotenv').config();
 
-app.get('/', (req, res) => {
-	res.send('Yes it works.');
-});
+// Set Up Routes
+const auth = require('./routes/auth');
 
+app.get('/', (req, res) => res.send('It works'));
+app.use('/auth', auth);
+
+// Run the app
 const port = process.env.PORT;
 app.listen(port, () => {
 	console.log(`App is running on port ${port} in ${nodeEnv} mode.`);
